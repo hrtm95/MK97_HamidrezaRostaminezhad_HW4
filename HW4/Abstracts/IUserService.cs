4384f3260ad9ea1812b5465d221ebf1f6da3d0f5
htrm95 <h.rostaminezhad1995@gmail.com>
2023-05-21 11:51:17 +0330

    make abstract file Iuserservice (crud)

---
+++ HW4/Abstracts/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using HW4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Abstracts
+{
+    public interface IUserService
+    {
+        List<Users> GetAllUser();
+        bool AddUser(Users user);
+        bool UpdateUser(Users user);
+        bool DeleteUser(Users user);
+    }
+}
