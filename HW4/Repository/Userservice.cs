b770fe3951c018d804d6b744b2988014258d3d26
htrm95 <h.rostaminezhad1995@gmail.com>
2023-05-21 16:44:33 +0330

    add Getalluser metof  and add AddUsermetod and add SaveOnCsv for write on file

---
+++ HW4/Repository/Userservice.cs
@@ -0,0 +1,86 @@
+﻿using CsvHelper;
+using HW4.Abstracts;
+using HW4.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Repository
+{
+    public class Userservice : IUserService
+    {
+        public string filePath = "C:\\Users\\DeveloperPC\\Source\\Repos\\MK97_HamidrezaRostaminezhad_HW4\\HW4\\Files\\New Microsoft Excel Worksheet.csv";
+        public bool AddUser(Users user)
+        {
+            List<Users> users = new List<Users>();
+            users = GetAllUser();
+            var serarchuser = from  a in users
+                              where a.Email == user.Email
+                              select a.Email ;
+            if (serarchuser != null)
+            {
+                users.Add(user);
+                SaveOnCsv(users);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                
+        }
+
+        public bool DeleteUser(Users user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetAllUser()
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Users>();
+                if (records != null)
+                {
+                    return records.ToList();
+                }                    
+                else return null;
+            }
+            
+        }
+
+        public bool UpdateUser(Users user)
+        {
+            throw new NotImplementedException();
+        }
+        public bool SaveOnCsv(List<Users> users)
+        {
+            try
+            {
+                using (var writer = new StreamWriter(filePath))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteHeader<Users>();
+                    csv.NextRecord();
+                    foreach (var User in users)
+                    {
+                        csv.WriteRecord(User);
+                        csv.NextRecord();
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
