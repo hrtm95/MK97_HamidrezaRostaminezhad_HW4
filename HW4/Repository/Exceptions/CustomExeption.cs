cbd0c92fbae370ef75679e1ce03c38a02fe82efc
htrm95 <h.rostaminezhad1995@gmail.com>
2023-05-23 15:42:50 +0330

    make cousom erorr

---
+++ HW4/Repository/Exceptions/CustomExeption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Repository.Exceptions
+{
+    public class CustomExeption : Exception
+    {
+        public CustomExeption() : base("costom erorr") { }
+        public CustomExeption(string s) : base(s) { }
+        //throw new CustomExeption();
+
+    }
+}
