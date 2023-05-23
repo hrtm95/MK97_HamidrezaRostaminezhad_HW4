5246484b209bbe536699a064d2a0d7d39c3d9be8
htrm95 <h.rostaminezhad1995@gmail.com>
2023-05-23 13:37:48 +0330

    add arical metod in view for user

---
+++ HW4/View/UserMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.View
+{
+    public class UserMenu
+    {
+        public void Arival ()
+        {
+            Console.Clear ();
+            Console.WriteLine("Hello enter number plese enter a number:");
+            Console.WriteLine("1 - to show all users");
+            Console.WriteLine("2 - to add user");
+            Console.WriteLine("3 - to delete user");
+            Console.WriteLine("4 - to update profile user");
+
+        }
+    }
+}
