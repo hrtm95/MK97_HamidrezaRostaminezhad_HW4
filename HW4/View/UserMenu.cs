9bf3e5753febfe67de6ed4a28b636e59840263e4
htrm95 <h.rostaminezhad1995@gmail.com>
2023-05-23 13:41:42 +0330

    chenge arival to sattic metod and get Name of user

--- HW4/View/UserMenu.cs
+++ HW4/View/UserMenu.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace HW4.View
 {
-    public class UserMenu
+    public static class UserMenu
     {
-        public void Arival ()
+        public static void Arival (string UserName)
         {
             Console.Clear ();
-            Console.WriteLine("Hello enter number plese enter a number:");
+            Console.WriteLine($"Hello {UserName} enter number plese enter a number:");
             Console.WriteLine("1 - to show all users");
             Console.WriteLine("2 - to add user");
             Console.WriteLine("3 - to delete user");
