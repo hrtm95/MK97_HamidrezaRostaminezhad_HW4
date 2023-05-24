b666f0b0e17208842db187c9b54febe3dc00d218
htrm95 <h.rostaminezhad1995@gmail.com>
2023-05-24 15:07:45 +0330

    add show all user in func 1

--- HW4/Views/Program.cs
+++ HW4/Views/Program.cs
@@ -13,44 +13,92 @@ namespace HW4
     {
         static void Main(string[] args)
         {
+            Userservice userservice = new Userservice();
             //this while is for exit from program 
             while (true)
             {
                 UserMenu.loginOrRegester();
-                string funcnumber = Console.ReadLine();
-                int func;
-                if (int.TryParse(funcnumber, out func))
+                string loginnumber = Console.ReadLine();
+                int LoginN;
+                if (int.TryParse(loginnumber, out LoginN))
                 {
-                    if (func == 1)
+                    if (LoginN == 1)
                     {
-                        Users user = new Users();
+
                         UserMenu.loginGetUsername();
                         string Email = Console.ReadLine();
                         UserMenu.loginGetPassword();
                         string Password = Console.ReadLine();
-                        UserMenu.Arival("hamidreza");
-                    }
-                    if (func == 2)
-                    {
+                        Users user = userservice.LoginUser(Email, Password);
+                        if (user != null)
+                        {
+                            UserMenu.Arival(user.Name);
+                            string UserFanc = Console.ReadLine();
+                            int UserFancNum;
+                            if (int.TryParse(UserFanc, out UserFancNum))
+                            {
+                                if (UserFancNum == 1)
+                                {
+                                    List<Users> users = userservice.GetAllUser();
+                                    if (users != null)
+                                    {
+                                        foreach (Users user1 in users)
+                                        {
+                                            Console.WriteLine($"ID: {user1.Id}\t Name:{user1.Name}\t LastName:{user1.Lastname}\t Email:{user1.Email}\t ");
+                                        }
+                                        UserMenu.WatingforContinue();
+                                    }
+                                    else
+                                    {
+                                        //null file eroor
+                                    }
+                                }
+                                else if (UserFancNum == 2)
+                                {
 
+                                }
+                                else if (UserFancNum == 3)
+                                {
+
+                                }
+                                else if (UserFancNum == 4)
+                                {
+
+                                }
+                                else
+                                {
+                                    //erro func 
+                                }
+                            }
+                            else
+                            {
+                                //error func
+                            }
+
+                        }
+                        if (LoginN == 2)
+                        {
+                            // Rigester just in User admin service
+                            // for regester new users : Coming soon :D
+                        }
+                        else
+                        {
+                            //eroor loign
+                        }
                     }
                     else
                     {
-                        //eroor loign
+                        //erorr login 
                     }
+
+
+
+
                 }
-                else
-                {
-                    //erorr login 
-                }
 
-
-
-
+                Console.ReadKey();
+                Console.ReadKey();
             }
-
-            Console.ReadKey();
-            Console.ReadKey();
         }
     }
 }
