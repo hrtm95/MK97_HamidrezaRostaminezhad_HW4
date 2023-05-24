5ae3c8d310df636672e1bd471189b03f7185902d
htrm95 <h.rostaminezhad1995@gmail.com>
2023-05-24 11:54:59 +0330

    add password in users.cs

--- HW4/Entitys/Users.cs
+++ HW4/Entitys/Users.cs
@@ -13,6 +13,7 @@ namespace HW4.Models
         public string Lastname { get; set; }
         public int RoleId { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string Mobile { get; set; }
         public DateTime BirthDate { get; set;}
 
